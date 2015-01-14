7c233fb19a8253ce49181294dd64e7ff31d82491
Jeroen Hooyberghs <jeroen@hooyberghs.org>
2015-01-14 09:42:15 +0100

    tabs to spaces

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,41 +2,41 @@
 #
 
 class sslh (
-	$run						= 'yes',
-	$daemon         = $sslh::params::daemon,
-	$config         = $sslh::params::config,
-	$template				= $sslh::params::template,
-	$pidfile        = $sslh::params::pidfile,
-	$user						= $sslh::params::user,
-	$port_sslh			= $sslh::params::port_sslh,
-	$port_ssl		  	= $sslh::params::port_ssl,
-	$port_http		  = $sslh::params::port_http,
-	$port_ssh			  = $sslh::params::port_ssh,
-	$port_openvpn   = $sslh::params::port_openvpn,
-	$port_xmpp	  	= $sslh::params::port_xmpp,
-	$port_tinc	  	= $sslh::params::port_tinc,
-	$listen_sslh    = '0.0.0.0',
-	$listen_ssl		  = undef,
-	$listen_http	  =	undef,
-	$listen_ssh		  = undef,
-	$listen_openvpn = undef,
-	$listen_xmpp		= undef,
-	$listen_tinc		= undef,
-	$supported_apps = ['ssl','http','ssh','openvpn','xmpp','tinc'],
+  $run            = 'yes',
+  $daemon         = $sslh::params::daemon,
+  $config         = $sslh::params::config,
+  $template       = $sslh::params::template,
+  $pidfile        = $sslh::params::pidfile,
+  $user           = $sslh::params::user,
+  $port_sslh      = $sslh::params::port_sslh,
+  $port_ssl       = $sslh::params::port_ssl,
+  $port_http      = $sslh::params::port_http,
+  $port_ssh       = $sslh::params::port_ssh,
+  $port_openvpn   = $sslh::params::port_openvpn,
+  $port_xmpp      = $sslh::params::port_xmpp,
+  $port_tinc      = $sslh::params::port_tinc,
+  $listen_sslh    = '0.0.0.0',
+  $listen_ssl     = undef,
+  $listen_http    =	undef,
+  $listen_ssh     = undef,
+  $listen_openvpn = undef,
+  $listen_xmpp    = undef,
+  $listen_tinc    = undef,
+  $supported_apps = ['ssl','http','ssh','openvpn','xmpp','tinc'],
 ) inherits sslh::params {
 
-	package { 'sslh':
-		ensure => installed,
-	} ~>
+  package { 'sslh':
+    ensure => installed,
+  } ~>
 
-	file { $config:
-		content => template($template)
-	} ~>
+  file { $config:
+    content => template($template),
+    notify  => Service['sslh'],
+  } ~>
 
-	service { 'sslh':
-		ensure => running,
-		enable => true,
-	}
-
+  service { 'sslh':
+    ensure => running,
+    enable => true,
+  }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,30 +4,31 @@
 #
 
 class sslh::params {
-	# general config
-	$port_sslh    = '443'
-	$port_ssl     = '443'
-	$port_http    = '80'
-	$port_ssh     = '22'
-	$port_openvpn = '1194'
-	$port_xmpp    = '5222'
-	$port_tinc    = '655'
-	$user					= 'sslh'
-	# OS specific config
+  # general config
+  $port_sslh    = '443'
+  $port_ssl     = '443'
+  $port_http    = '80'
+  $port_ssh     = '22'
+  $port_openvpn = '1194'
+  $port_xmpp    = '5222'
+  $port_tinc    = '655'
+  $user          = 'sslh'
+
+  # OS specific config
   case $::operatingsystem {
-		'ubuntu','debian': {
-			$daemon   = '/usr/sbin/sslh'
-			$config   = '/etc/default/sslh'
-			$pidfile  = '/var/run/sslh/sslh.pid'
-			$template = 'sslh/sslh-debian.erb'
-		}
-		#	'centos','fedora','rhel': {
-		#$daemon = '/usr/sbin/sslh'
-		#$config = '/etc/sysconfig/sslh'
-		#$pidfile = '/var/run/sslh.pid' 
-		#$template = 'sslh/sslh-centos.erb'
-		#}
-		#
-		default: { fail("${::operatingsystem} is currently not supported by this sslh puppet module") }
-	}
+    'ubuntu','debian': {
+      $daemon   = '/usr/sbin/sslh'
+      $config   = '/etc/default/sslh'
+      $pidfile  = '/var/run/sslh/sslh.pid'
+      $template = 'sslh/sslh-debian.erb'
+    }
+    #  'centos','fedora','rhel': {
+    #$daemon = '/usr/sbin/sslh'
+    #$config = '/etc/sysconfig/sslh'
+    #$pidfile = '/var/run/sslh.pid' 
+    #$template = 'sslh/sslh-centos.erb'
+    #}
+    #
+    default: { fail("${::operatingsystem} is currently not supported by this sslh puppet module") }
+  }
 }
